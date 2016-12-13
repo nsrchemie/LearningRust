890747c8d0d3a004c441d16d6c0cb837e92e835a
nsrchemie <nsrchemie@gmail.com>
2016-12-13 18:39:41 -0500

    vec repeating of value

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -3,7 +3,11 @@ fn main() {
 	//VECTORS
 	//Vectors are dynamic arrays, typically implemented as Vec<T>
 	//vectors always allocate on heap
-	let v = vec![1,2,3,4,5];
-	let slice = &v[0..3];
-	println!("{:?}",slice);
+	// let v = vec![1,2,3,4,5];
+	// let slice = &v[0..3];
+	// println!("{:?}",slice);
+
+	//to create a vector of duplicates
+	let v = vec![0; 10];
+	println!("{:?}",v);
 }
