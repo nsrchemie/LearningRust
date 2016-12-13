2d6d746faed719608e2f343d15350713f359d33c
nsrchemie <nsrchemie@gmail.com>
2016-12-13 18:01:49 -0500

    created concurrency practice example

--- cond_loops/src/main.rs
+++ cond_loops/src/main.rs
@@ -10,8 +10,28 @@ fn main() {
 
 	// Rewrite
 
-	let x = 5;
-	let y = if x==5 {10} else {15};
+	// let x = 5;
+	// let y = if x==5 {10} else {15};
 
-	println!("y is equal to {}",y);
+	// println!("y is equal to {}",y);
+
+
+
+	//loop can be used for situations like 'while true {}'
+
+	//loop { println!("This is an infinite loop!")};
+
+	//while loops
+	let mut x = 5;
+	let mut done = false;
+
+	while !done {
+		x += x -3;
+
+		println!("{}",x);
+
+		if x % 5 == 0 {
+			done = true;
+		}
+	}
 }
