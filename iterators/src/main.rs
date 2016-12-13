74510f6d65671dcde3e23a28c48ce77ac976d3b2
nsrchemie <nsrchemie@gmail.com>
2016-12-13 12:30:55 -0500

    Vector type inference example added

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
 
-// Consumer, returns values
-let one_to_one_hundred = (1..101).collect::<Vec<i32>>();
+// Consumer, returns values with explicit type declaration for Vector
+// let one_to_one_hundred = (1..101).collect::<Vec<i32>>();
+// Alternatively, can tell compiler to infer type of Vec
+let one_to_one_hundred = (1..101).collect::<Vec<_>>();
 let slice = &one_to_one_hundred[30..70];
 println!("{:?}",slice);
 }
