9d1b1fb9000e1867a372bea311735673e12f0b46
nsrchemie <nsrchemie@gmail.com>
2016-12-13 12:32:48 -0500

    Working with find consumer

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -3,7 +3,11 @@ fn main() {
 // Consumer, returns values with explicit type declaration for Vector
 // let one_to_one_hundred = (1..101).collect::<Vec<i32>>();
 // Alternatively, can tell compiler to infer type of Vec
-let one_to_one_hundred = (1..101).collect::<Vec<_>>();
-let slice = &one_to_one_hundred[30..70];
-println!("{:?}",slice);
+// let one_to_one_hundred = (1..101).collect::<Vec<_>>();
+// let slice = &one_to_one_hundred[30..70];
+
+
+//Find Consumer
+let over_twenty = (0..80).find(|x| *x > 20);
+println!("{:?}",over_twenty);
 }
