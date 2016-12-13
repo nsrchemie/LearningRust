6ace0e7ce69630e433ce7be793a9bb3def22e6dc
nsrchemie <nsrchemie@gmail.com>
2016-12-13 18:34:06 -0500

    created vectors project

--- cond_loops/src/main.rs
+++ cond_loops/src/main.rs
@@ -18,20 +18,71 @@ fn main() {
 
 
 	//loop can be used for situations like 'while true {}'
-
 	//loop { println!("This is an infinite loop!")};
 
-	//while loops
-	let mut x = 5;
-	let mut done = false;
+	//WHILE LOOPS
+	// let mut x = 5;
+	// let mut done = false;
+	// while !done {
+	// 	x += x -3;
+	// 	println!("{}",x);
+	// 	if x % 5 == 0 {
+	// 		done = true;}}
 
-	while !done {
-		x += x -3;
+	//for loop with range (end number of ranges are exclusive)
+	// for x in 0..10 {
+	// 	println!("{}",x);
+	// }
 
-		println!("{}",x);
+	//enumerate helps keep track of how many times you have already looped
+	//enumerate on a range
+	// for (index, value) in (5..10).enumerate() {
+	// 	println!("Index = {} and value = {}",index, value);
+	// }
 
-		if x % 5 == 0 {
-			done = true;
+	//enumerate on iterators
+	// let lines = "hello\nworld".lines();
+
+	// for (line_number, line) in lines.enumerate() {
+	// 	println!("{}: {}", line_number, line);
+	// }
+
+
+
+
+
+
+
+	//DISRUPTING ITERATION/ENDING ITERATION EARLY
+	//USING BREAK
+	//this is a slight rewrite of the while example starting on line 23
+	// let mut x = 5;
+
+	// loop {
+	// 	x += x - 3;
+
+	// 	println!("{}",x);
+
+	// 	if x % 5 == 0 {break;}
+	// }
+
+	//USING CONTINUE
+	//this prints odd numbers
+	// for x in 0..10 {
+	// 	if x % 2 == 0 {continue;}
+	// 	println!("{}",x);
+	// }
+
+	//LOOP LABELS
+
+	'outer: for x in 0..10 {
+		'inner: for y in 1..10 {
+			if x % 2 == 0 { continue 'outer;}
+			if y % 2 == 0 { continue 'inner;}
+			println!("x: {}, y: {}",x, y);
 		}
 	}
+
+
+
 }
