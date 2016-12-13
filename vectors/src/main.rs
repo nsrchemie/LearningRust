aab1fbc2d625408e81d5a3bd3ebfaf5f90272297
nsrchemie <nsrchemie@gmail.com>
2016-12-13 18:38:32 -0500

    simple vector initialization and slicing

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+
+	//VECTORS
+	//Vectors are dynamic arrays, typically implemented as Vec<T>
+	//vectors always allocate on heap
+	let v = vec![1,2,3,4,5];
+	let slice = &v[0..3];
+	println!("{:?}",slice);
 }
