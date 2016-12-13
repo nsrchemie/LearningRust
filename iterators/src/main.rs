fed36f800571439c0ed44fef4a0b8b0eb9b52180
nsrchemie <nsrchemie@gmail.com>
2016-12-13 12:05:16 -0500

    slice of collected consumer

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -2,6 +2,6 @@ fn main() {
 
 // Consumer, returns values
 let one_to_one_hundred = (1..101).collect::<Vec<i32>>();
-println!("{:?}",one_to_one_hundred);
-
+let slice = &one_to_one_hundred[30..70];
+println!("{:?}",slice);
 }
