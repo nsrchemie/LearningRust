d81eec00a0585aa9a57cf24d17bd3217de09ddbd
nsrchemie <nsrchemie@gmail.com>
2016-12-13 14:18:45 -0500

    chaining numerous adapters together

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -62,5 +62,10 @@ fn main() {
 // 	println!("{}",i);
 // }
 
-
+let chained = (1..)
+	.filter(|&x| x % 2 == 0)
+	.filter(|&x| x % 3 == 0)
+	.take(5)
+	.collect::<Vec<i32>>();
+println!("{:?}",chained);
 }
