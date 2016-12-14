9917daccbb0ff6cd039ceb6d6775e94690e10d0d
nsrchemie <nsrchemie@gmail.com>
2016-12-14 11:28:24 -0500

    showing mutability in structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -7,6 +7,13 @@ fn main() {
 		y: i32,
 	}
 //create instance
-let origin = Point {x:0, y:0};
-println!("x:{} y:{}",origin.x,origin.y);
+// let origin = Point {x:0, y:0};
+
+//create instance with mutability, you cant add mut to the definition
+//of struct parameters like 
+// Point {mut x: i32 --> THIS WILL NOT WORK
+	//The below works
+	let mut point = Point {x:0,y:0};
+	point.x = 5;
+println!("x:{} y:{}",point.x,point.y);
 }
