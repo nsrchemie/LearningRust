4715211b031f8aa5507a7d3111fabc0e73261b49
nsrchemie <nsrchemie@gmail.com>
2016-12-13 19:15:39 -0500

    indexing vectors

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -8,6 +8,13 @@ fn main() {
 	// println!("{:?}",slice);
 
 	//to create a vector of duplicates
-	let v = vec![0; 10];
-	println!("{:?}",v);
+	// 	let v = vec![0; 10];
+	// 	println!("{:?}",v);
+
+
+
+	//indexing
+	let v = vec![1,2,3];
+	println!("The first element of v is {}",v[0]);
+
 }
