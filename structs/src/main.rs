3bf67b659033d73195de6f46fd01d8727992f65a
nsrchemie <nsrchemie@gmail.com>
2016-12-14 11:25:30 -0500

    simple struct instance creation

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+	//STRUCTS
+	//useful for more complex data types
+
+	struct Point {
+		x: i32,
+		y: i32,
+	}
+//create instance
+let origin = Point {x:0, y:0};
+println!("x:{} y:{}",origin.x,origin.y);
 }
