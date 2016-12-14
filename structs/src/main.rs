86a3a83d9235623f5221d8e31211277b0a86e255
nsrchemie <nsrchemie@gmail.com>
2016-12-14 13:33:30 -0500

    emulating field mutability in structs with use of std Cell

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,11 +1,13 @@
+use std::cell::Cell;
+
 fn main() {
 	//STRUCTS
 	//useful for more complex data types
 
-	struct Point {
-		x: i32,
-		y: i32,
-	}
+	// struct Point {
+	// 	x: i32,
+	// 	y: i32,
+	// }
 //create instance
 // let origin = Point {x:0, y:0};
 
@@ -13,7 +15,22 @@ fn main() {
 //of struct parameters like 
 // Point {mut x: i32 --> THIS WILL NOT WORK
 	//The below works
-	let mut point = Point {x:0,y:0};
-	point.x = 5;
-println!("x:{} y:{}",point.x,point.y);
+// 	let mut point = Point {x:0,y:0};
+// 	point.x = 5;
+// println!("x:{} y:{}",point.x,point.y);
+
+//Can emulate field mutability by using Cell<T>
+
+// struct Point {
+// 	x: i32,
+// 	y: Cell<i32>,
+// }
+
+// let point = Point {x: 5, y: Cell::new(6)};
+// println!("y {:?}", point.y);
+// point.y.set(7);
+
+// println!("y has been changed to {:?}", point.y);
+
+
 }
