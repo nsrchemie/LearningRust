be8a75ef211c6310426fb63ab8ccb2fda836f914
nsrchemie <nsrchemie@gmail.com>
2016-12-15 12:20:30 -0500

    basic enum definition

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+	//enums are useful for mixing datatypes 
+	//and variants dont necessarily need data associated
+	//Unlike structs, enums are a single type
+	enum Message {
+		Quit,
+		ChangeColor(i32, i32, i32),
+		Move {x: i32, y: i32 },
+		Write(String),
+	}
+
+	//working with enums
 }
