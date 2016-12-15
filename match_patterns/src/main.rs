5e38d36ca58cf5546348376c2a3c67c8d10dd6bd
nsrchemie <nsrchemie@gmail.com>
2016-12-15 17:48:46 -0500

    selective value grabbing in destructuring

--- match_patterns/src/main.rs
+++ match_patterns/src/main.rs
@@ -12,6 +12,8 @@ fn main() {
 	match origin {
 		// Point { x, y } => println!("({}, {})",x,y),
 		//Can also rename values
-		Point { x: x1, y: y1 } => println!("({}, {})",x1,y1),
+		// Point { x: x1, y: y1 } => println!("({}, {})",x1,y1),
+		//You can selectively choose values
+		Point {y, .. } => println!("y is {}",y),
 	}
 }
