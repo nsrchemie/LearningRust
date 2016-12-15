38bd9309dd11ab5734382659ff6bddbecf7e0253
nsrchemie <nsrchemie@gmail.com>
2016-12-15 18:18:13 -0500

    string newline demonstration

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -5,5 +5,10 @@ fn main() {
     //string literals are statically allocated
     //they exist for the entire duration they run
 
-
+    //two forms of strings spanning multiple lines
+    let s = "foo
+    bar";
+    //the assert has the newline and 
+    //any leading whitespace (4 spaces)
+    assert_eq!("foo\n    bar", s);
 }
