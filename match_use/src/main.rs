af7c5ae64b636412af42fb5b6c52ff411dc44c89
nsrchemie <nsrchemie@gmail.com>
2016-12-15 16:04:23 -0500

    using match with enums, showing exhaustiveness still required

--- match_use/src/main.rs
+++ match_use/src/main.rs
@@ -13,11 +13,33 @@ fn main() {
 	//match can also be used in a let binding 
 	//to convert between types 
 
-	let number = match x {
-		1 => "one",
-		2 => "two",
-		_=> "Other number",
-	};
+	// let number = match x {
+	// 	1 => "one",
+	// 	2 => "two",
+	// 	_=> "Other number",
+	// };
 
-	println!("{}",number);
+	// println!("{}",number);
+
+	//MATCHING ON ENUMS
+
+	enum Message {
+		Quit,
+		ChangeColor(i32,i32,i32),
+		Move {x: i32, y:i32},
+		Write(String),
+	}
+
+fn quit() {/* ... */}
+fn change_color(r: i32,g: i32, b: i32){/* ... */}
+fn move_cursor(x:i32, y:i32){/*...*/}
+
+fn process_message(msg: Message) {
+	match msg {
+		Message::Quit => quit(),
+		Message::ChangeColor(r,g,b) => change_color(r,g,b),
+		Message::Move {x,y} => move_cursor(x,y),
+		Message::Write(s) => println!("{}",s),
+	};
+}
 }
