fdff899056fa809fbe91934c8f10a90e80c7b1bf
nsrchemie <nsrchemie@gmail.com>
2016-12-15 14:39:19 -0500

    simple match statement

--- match_use/src/main.rs
+++ match_use/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+
+	//match is used for exhaustive conditionals
+
+	let x = 5;
+
+	match x {
+		1 => println!("One"),
+		2 => println!("Two"),
+		_ => println!("Value above 2"),
+	}
 }
