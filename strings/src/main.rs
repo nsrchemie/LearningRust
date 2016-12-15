571d954e9c2dfb7fd52e2ea173a3531fca5720b2
nsrchemie <nsrchemie@gmail.com>
2016-12-15 18:31:20 -0500

    working with String types, showing mutability

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
+	//Strings are &str and String
+	//&str are string slices
     // let greeting = "Hello!";
     //The above is a string literal and its type is
     //&'static str
     //string literals are statically allocated
     //they exist for the entire duration they run
+    //They are NOT MUTABLE
 
     //two forms of strings spanning multiple lines
     // let s = "foo
@@ -13,9 +16,25 @@ fn main() {
     // assert_eq!("foo\n    bar", s);
 
 
-    //You can trim spaces AND newline
-    let s = "foo\
-              bar";
+    // //You can trim spaces AND newline
+    // let s = "foo\
+    //           bar";
 
-    assert_eq!("foobar",s);
+    // assert_eq!("foobar",s);
+
+
+    //Accessing str's can only be done through &str reference
+    //str is an unsized type, require addtl runtime info to 
+    //be used
+    
+    //String is a heap-allocated string
+    //This makes it growable, and guaranteed UTF-8
+    //To create a String, convert a string  slice using
+    //to_string method
+
+    let mut s = "Hello".to_string();
+    println!("{}",s);
+
+    s.push_str(", world");
+    println!("{}",s);
 }
