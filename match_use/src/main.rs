02a96d88f4c583b8e17ec043b089f055f424e2b6
nsrchemie <nsrchemie@gmail.com>
2016-12-15 14:53:20 -0500

    binding match evaluation to variable, shows ability to switch between types

--- match_use/src/main.rs
+++ match_use/src/main.rs
@@ -2,11 +2,22 @@ fn main() {
 
 	//match is used for exhaustive conditionals
 
-	let x = 5;
+	// let x = 5;
 
-	match x {
-		1 => println!("One"),
-		2 => println!("Two"),
-		_ => println!("Value above 2"),
-	}
+	// match x {
+	// 	1 => println!("One"),
+	// 	2 => println!("Two"),
+	// 	_ => println!("Value above 2"),
+	// }
+
+	//match can also be used in a let binding 
+	//to convert between types 
+
+	let number = match x {
+		1 => "one",
+		2 => "two",
+		_=> "Other number",
+	};
+
+	println!("{}",number);
 }
