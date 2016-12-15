f6d5f5bac9490a0954c42f33233680bf370491e2
nsrchemie <nsrchemie@gmail.com>
2016-12-15 18:15:23 -0500

    simple string literal

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    // let greeting = "Hello!";
+    //The above is a string literal and its type is
+    //&'static str
+    //string literals are statically allocated
+    //they exist for the entire duration they run
+
+
 }
