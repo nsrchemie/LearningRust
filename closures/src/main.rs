f5e0c4ff38c551685931717ff5c47a3b32a23802
nsrchemie <nsrchemie@gmail.com>
2016-12-15 20:40:35 -0500

    closures and scope

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -21,6 +21,18 @@ fn main() {
 	// println!("{}",plus_two(1));
 
 	//You can simplify closures
-	let plus_one = |x: i32| -> i32 {x + 1};
-	println!("{}", plus_one(1));
+	// let plus_one = |x: i32| -> i32 {x + 1};
+	// println!("{}", plus_one(1));
+
+	//closures and scope
+	// let mut num = 5;
+	// let plus_num = |x: i32| x + num;
+	// //The above closure is borrowing num
+
+	//Use curly braces to help with scope
+	let mut num = 5;
+	{
+	let plus_num = |x: i32| x + num;
+	}
+	let y = &mut num;
 }
