b3f5ff2764b176f469e08817cf2503b7e91b8d10
nsrchemie <nsrchemie@gmail.com>
2016-12-15 20:32:32 -0500

    multi line closure

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -5,6 +5,18 @@ fn main() {
 	//wrap up a function and free variables
 
 	//simple closure 
-	let plus_one = |x: i32| x + 1;
-	println!("{}",plus_one(1));
+	// let plus_one = |x: i32| x + 1;
+	// println!("{}",plus_one(1));
+
+	//Multi-line closure
+	let plus_two = |x| {
+		let mut result: i32 = x;
+
+		result += 1;
+		result += 1;
+
+		result
+	};
+
+	println!("{}",plus_two(1));
 }
