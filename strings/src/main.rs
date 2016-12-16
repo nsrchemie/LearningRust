9f8cc10b55cc767061f01acb9877a531fa3edfc3
nsrchemie <nsrchemie@gmail.com>
2016-12-15 19:33:59 -0500

    slicing borrowed strings

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -60,14 +60,20 @@ fn main() {
     //can look at string as bytes or codepoints
 
     let me = "Nicholas";
-    // for v in me.chars() {
-    // 	println!("{}",v);
-    // }
-    // for b in me.as_bytes() {
-    // 	println!("{}",b);
-    // }
+    // // for v in me.chars() {
+    // // 	println!("{}",v);
+    // // }
+    // // for b in me.as_bytes() {
+    // // 	println!("{}",b);
+    // // }
 
-    //Closest equivalent to indexing is nth method with chars
-    let first = me.chars().nth(0);
-    println!("{:?}", first);
+    // //Closest equivalent to indexing is nth method with chars
+    // let first = me.chars().nth(0);
+    // println!("{:?}", first);
+
+    //Can slice a string with &str[int..int]
+    let mo = &me[0..5];
+    println!("{}",mo);
+
+
 }
--- strings/target/debug/strings.d
+++
@@ -1,5 +0,0 @@
-/home/nsrchemie/Documents/rustprac/strings/target/debug/strings: src/main.rs
-
-/home/nsrchemie/Documents/rustprac/strings/target/debug/strings.d: src/main.rs
-
-src/main.rs:
