004997f4384beb8b62be131c59bf7997059a8168
nsrchemie <nsrchemie@gmail.com>
2016-12-15 21:02:49 -0500

    demonstrated real test using local closure and public function with assert

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,8 +1,16 @@
 #[test]
 #[should_panic(expected = "assertion failed")]
 fn it_works() {
-	assert_eq!(2,1);
+
+	let add_one = |x: i32| x + 1;
+	assert_eq!(1,add_one(1));
+}
+
+
+pub fn add_two(a: i32) -> i32 {
+a + 2
 }
 
 fn main() {
+
 }
