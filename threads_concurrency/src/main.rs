f07c825adce09e2b6c8f006edd1bdccf70bd8bc4
nsrchemie <nsrchemie@gmail.com>
2016-12-16 14:49:24 -0500

    basic thread spawn

--- threads_concurrency/src/main.rs
+++ threads_concurrency/src/main.rs
@@ -1,3 +1,13 @@
+//standard library has threads for running parallel code
+use std::thread;
+
 fn main() {
-	
+	//spawn method accepts a closure to execute in
+	//a new thread
+	//It returns a handle used to wait for the 
+	//child thread to finish
+	thread::spawn(|| {
+		println!("This is inside a thread");
+	});
+
 }
