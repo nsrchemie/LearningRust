3abdd256a0ba74f606cd46189cb5a506bec3c3c2
nsrchemie <nsrchemie@gmail.com>
2016-12-16 14:59:55 -0500

    use of move closure to own environment variable inside thread

--- threads_concurrency/src/main.rs
+++ threads_concurrency/src/main.rs
@@ -6,9 +6,24 @@ fn main() {
 	//a new thread
 	//It returns a handle used to wait for the 
 	//child thread to finish
-	let handle = thread::spawn(|| {
-		"This is inside a thread"
-	});
+	// let handle = thread::spawn(|| {
+	// 	"This is inside a thread"
+	// });
 
-	println!("{}",handle.join().unwrap());
+	// println!("{}",handle.join().unwrap());
+
+	//Keep in mind that closures by default take variable 
+	//references, this can cause an error since 
+	//the thread could potentially outlive the references
+	
+	//use a move closure to force closure to take ownership
+	//move closures move variables into them from the env.
+	let x  = 1;
+	let handle = thread::spawn(move || {
+		println!("x is {}",x);
+	});
+	//extra code below for me to see inside thread
+	//unwrap used to hide Ok/Err and show value
+	//skipping use
+	println!("{:?}",handle.join());
 }
