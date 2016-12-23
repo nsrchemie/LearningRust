7163c381aa8a377427291a06ce2af6b955bd2c34
nsrchemie <nsrchemie@gmail.com>
2016-12-23 15:03:00 -0500

    showing structure constructor method

--- struct_impl/src/main.rs
+++ struct_impl/src/main.rs
@@ -1,5 +1,21 @@
 fn main() {
 
+	//Demonstrating declaration of structs
+	struct Rectangle {
+		height: f64,
+		width: f64
+	}
+
+	//we can define struct methods using impl
+	impl Rectangle {
+		//Creating a static method (Constructor), 
+		//keep in mind the name 'new' can be anything
+		fn new(h: f64, w: f64) -> 
+		Rectangle {
+			height: h,
+			width: w
+		}
+	}
 
 }
 
