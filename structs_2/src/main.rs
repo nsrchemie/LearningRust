261f2bad7186af1e62da06d847f441c0d92a78ac
nsrchemie <nsrchemie@gmail.com>
2017-01-22 15:22:54 -0500

    showing the use of constraints in struct constructor methods

--- structs_2/src/main.rs
+++ structs_2/src/main.rs
@@ -5,7 +5,19 @@ fn main() {
 		health: u32,
 		damage: u32
 	}
+	// let mut bork = Alien{health: 100, damage: 5};
+	// println!("{}",bork.health);
 
-	let mut bork = Alien{health: 100, damage: 5};
+	//Let's say the health parameter cannot exceed 100
+	//we can make this limitation in a method
+	impl Alien {
+		fn new(mut h: u32, d: u32) ->Alien {
+		if h > 100 {h = 100; }
+		Alien {health:h, damage:d}
+	}
+}
+	//Now we have permanently set an upper limit for health
+	//and also simplified the initializing command
+	let mut bork = Alien::new(150,15);
 	println!("{}",bork.health);
 }
